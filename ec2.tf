80c8b4eb62e69591861636c8d4e938ca5474de61
pankaj <pankajindevops@gmail.com>
2022-09-18 11:24:18 +0100

    File added

--- ec2.tf
+++ ec2.tf
@@ -61,6 +61,8 @@ resource "aws_key_pair" "key-pair-01" {
   # This works - public_key = "key-pair-01" // ie. the actual file name
   
   // public_key = "/home/pankajsharma/terraform/keys/key-pair-01.pub"
-  public_key = "${file("/home/pankajsharma/terraform/keys/key-pair-01.pub")}"
+  // public_key = "${file("/home/pankajsharma/terraform/keys/key-pair-01.pub")}"
+  
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDAZixG2lccPNViHAWLGWXJTlba6xKHrYj83xxq5V+ycIY18IbSF+jC1sNvreLQv477CHDOV3DnezYLFWSnYCXMGaNwOl4uftSyN1Exdliybr1/mcreriOwn4K24g9tyWhbWa0XRwPZfpVGQiXeYLjZkbCaL3OoAh+CTrEWFW40Zu7eupUil/YZEItYPnH4qxA6y+45q1Zbttr2GyfUlSbbO/fHGvm0MloMcW5ROHQvlYQTGoiewka9BYrYGPpseQiH8ztylIz+g9xaWSMYR0dYepbb+b+CON2xdAp0itlFnW9UK7vhDnv7ZTzJylyPlxnEKXQ2zR4o5dpkP0L6D4Y4ixqkAo8mmQQ3FMIv/blYaExO8S3B3sZMXKOObx/zqP330EyKf63s0PF2H/DGVckJAWz0jAfHIzxu4WQmZ7uSWTxzd09NHEMTPxjEA9Q42l1FQHNyMiDuTcfK5j8EJAGUwTNKwVUvuV8B+x33k8TsJ9TwgS4D8TuFlseGE93O6Zc= pankajsharma@pankajsharma-Inspiron-16-5625"
 
 }
